8420c3ab173870814902c5949c6e1d182c36dea2
tinyhill <tinyhill@163.com>
2013-06-21 14:51:59 +0800

    .gitignore

--- admin/application/controllers/Home.php
+++ admin/application/controllers/Home.php
@@ -7,9 +7,9 @@ class Home extends CI_Controller {
 
 	function index () {
 
-		$this->load->model('module');
-		$result = $this->module->download('http://cms.taobao.com/dpl/api/module/1408c02d-63a5-40cd-85a7-d5624a4e894f/index.htm', array(
-			'id' => '1408c02d-63a5-40cd-85a7-d5624a4e894f'
+		$this->load->model('get');
+		$result = $this->get->_template_common(array(
+			'filter' => 'server'
 		));
 		var_dump($result);
 
--- admin/application/models/market.php
+++ admin/application/models/market.php
@@ -5,10 +5,27 @@
  */
 class Market extends CI_Model {
 
-	// 查询市场
-	function select ($args) {
+	// 创建市场
+	function create ($args) {
 
-		// todo
+		$this->load->library('dir');
+
+		// 配置基础路径
+		$www_dir = $this->config->item('www');
+		$market_dir = $www_dir . '/' . $args['market'] . '/';
+
+		// 创建市场根目录
+		if (!file_exists($www_dir)) {
+			@mkdir($www_dir, 0777);
+		}
+
+		// 创建市场目录
+		if (!file_exists($market_dir)) {
+			@mkdir($market_dir, 0777);
+		}
+
+		// 写入页面组件
+		http://www.taobao.com/go/market/5341/__header.php
 
 	}
 
