54a555a96bd2703a879d8b517e6cef6811031081
tinyhill <tinyhill@163.com>
2013-07-09 00:34:53 +0800

    版本修订

--- admin/application/controllers/home.php
+++ admin/application/controllers/home.php
@@ -8,7 +8,7 @@ class Home extends CI_Controller {
 	function index () {
 
 		$args = array(
-			'title' => '多业务规范版 &lsaquo; DPL',
+			'title' => '多终端版 &lsaquo; DPL',
 			'version' => $this->config->item('version'),
 			'setting' => $this->config->item('setting')
 		);
