b7edec2e0fc751a1f9af0f06ae989324bcdd2463
tinyhill <tinyhill@163.com>
2013-07-15 10:30:28 +0800

    新增运行环境信息

---
+++ admin/application/controllers/phpinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 运行环境控制器
+ */
+class Phpinfo extends CI_Controller {
+
+	function index () {
+
+		phpinfo();
+
+	}
+
+}
--- admin/application/views/home.php
+++ admin/application/views/home.php
@@ -3,6 +3,7 @@
 		<div class="hd"><span>系统概况</span></div>
 		<div class="bd">
 			<ul class="clearfix">
+				<li>运行环境：PHP <?= PHP_VERSION ?>（<span><?= PHP_SAPI ?></span>）<a href="<?= base_url('phpinfo') ?>">查看详情</a></li>
 				<li>程序版本：多终端版（<span id="version"><?= $version ?></span>）<span id="message"></span></li>
 				<li>业务规范：<?= $setting['name'] ?>（<?= $setting['company'] ?>）<a href="<?= base_url('setting') ?>">点此修改</a></li>
 			</ul>
