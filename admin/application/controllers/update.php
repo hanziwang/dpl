469a75c54b7d0cd1af817690601a3ce5405934e4
tinyhill <tinyhill@163.com>
2013-07-17 00:53:37 +0800

    bugfix

--- admin/application/controllers/update.php
+++ admin/application/controllers/update.php
@@ -7,7 +7,6 @@ class Update extends CI_Controller {
 
 	function index () {
 
-		$this->load->model('get');
 		$args = array(
 			'title' => '更新业务数据 &lsaquo; DPL',
 			'version' => $this->config->item('version')
