6a660990b976fec874cc77a5e77c7af150c06186
tinyhill <tinyhill@163.com>
2013-08-21 11:59:07 +0800

    20130821

--- admin/application/config/vars.php
+++ admin/application/config/vars.php
@@ -15,7 +15,7 @@ $config['tms_type'] = $tms . 'client/getModuleTypes.htm';
 $config['tms_author'] = $tms . 'client/getModuleAuthors.htm';
 
 // 配置系统参数
-$config['version'] = '20130813';
+$config['version'] = '20130821';
 
 // 配置系统路径
 $home = dirname(dirname(BASEPATH));
